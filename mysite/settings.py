9484e3614364ff084d8a769ee74bbe081dbcf565
hortmagen21 <max_yudkin@ukr.net>
2021-09-22 22:01:17 +0300

    cors add link to connect front to back

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,11 +41,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
+    "corsheaders",
     'rest_framework',
 
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 
@@ -137,6 +140,10 @@ STATICFILES_DIRS = (
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django.setup()
